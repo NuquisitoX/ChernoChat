a7b9a724f3d00629af9a007e08880e72b720dccf
NuquisitoX <nordin_v_nes@hotmail.com>
2014-02-11 21:02:04 +0100

    second

---
+++ src/com/TheCherno/ChernoChat/Client.java
@@ -0,0 +1,122 @@
+package com.TheCherno.ChernoChat;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class Client extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	private JPanel contentPane;
+
+	private String name;
+	private String address;
+	private int port;
+
+	private JTextField txtMessage;
+	private JTextArea txtHistory;
+	
+	
+
+	public Client(String name, String address, int port) {
+		setTitle("Cherno Chat - " + name);
+		this.name = name;
+		this.address = address;
+		this.port = port;
+		boolean connect = openConnection(address, port);
+		createWindow();
+		if(!connect){
+			System.err.println("Connection failed!");
+			console("Connection failed!");
+		} else {
+			console("Attempting a connection to " + address + ":" + port + ", user: " + name);
+		}
+	}
+
+	private boolean openConnection(String address, int port) {
+		return false;
+	}
+
+	private void createWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(880, 550);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 28, 815, 30, 7 };
+		gbl_contentPane.rowHeights = new int[] { 35, 475, 40 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, 1.0 };
+		gbl_contentPane.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		txtHistory = new JTextArea();
+		txtHistory.setEditable(false);
+		JScrollPane scroll = new JScrollPane(txtHistory);
+		GridBagConstraints gbc_scroll = new GridBagConstraints();
+		gbc_scroll.insets = new Insets(0, 5, 5, 5);
+		gbc_scroll.fill = GridBagConstraints.BOTH;
+		gbc_scroll.gridx = 0;
+		gbc_scroll.gridy = 0;
+		gbc_scroll.gridwidth = 3;
+		gbc_scroll.gridheight = 2;
+		contentPane.add(scroll, gbc_scroll);
+
+		txtMessage = new JTextField();
+		txtMessage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				send(txtMessage.getText());
+			}
+		});
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 5, 5, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 0;
+		gbc_txtMessage.gridy = 2;
+		gbc_txtMessage.gridwidth = 2;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+
+		JButton btnSend = new JButton("Send");
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				send(txtMessage.getText());
+			}
+		});
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSend.gridx = 2;
+		gbc_btnSend.gridy = 2;
+		contentPane.add(btnSend, gbc_btnSend);
+
+		setVisible(true);
+		txtMessage.requestFocusInWindow();
+	}
+
+	private void send(String message) {
+		if (message.equals("") || message.length() == 0)
+			return;
+		message = name + ": " + message;
+		console(message);
+		txtMessage.setText("");
+	}
+
+	public void console(String message) {
+		txtHistory.append(message + "\n\r");
+		txtHistory.setCaretPosition(txtHistory.getDocument().getLength());
+	}
+
+}
--- src/com/TheCherno/ChernoChat/Login.java
+++ src/com/TheCherno/ChernoChat/Login.java
@@ -1,8 +1,112 @@
 package com.TheCherno.ChernoChat;
 
-public class Login {
-	public static void main(String[] args){
-		System.out.println("Hello!");
-		System.out.println("See Ya!");
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
+
+public class Login extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	private JPanel contentPane;
+	private JTextField txtName;
+	private JTextField txtAddress;
+	private JTextField txtPort;
+	private JLabel lblName;
+	private JLabel lblIpAddress;
+	private JLabel lblPort;
+	private JLabel lblAddressDesc;
+	private JLabel lblPortDesc;
+	private JButton btnLogin;
+
+	public Login() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		setResizable(false);
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 380);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		lblName = new JLabel("Name:");
+		lblName.setBounds(127, 34, 45, 16);
+
+		txtName = new JTextField();
+		txtName.setBounds(67, 50, 165, 28);
+		txtName.setColumns(10);
+
+		lblIpAddress = new JLabel("IP Address:");
+		lblIpAddress.setBounds(111, 96, 77, 16);
+
+		txtAddress = new JTextField();
+		txtAddress.setBounds(67, 116, 165, 28);
+		txtAddress.setColumns(10);
+
+		lblAddressDesc = new JLabel("(eg. 192.168.0.2)");
+		lblAddressDesc.setBounds(94, 142, 112, 16);
+
+		lblPort = new JLabel("Port:");
+		lblPort.setBounds(133, 171, 34, 16);
+
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(67, 191, 165, 28);
+
+		lblPortDesc = new JLabel("(eg. 8192)");
+		lblPortDesc.setBounds(116, 218, 68, 16);
+
+		btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = txtName.getText();
+				String address = txtAddress.getText();
+				int port = Integer.parseInt(txtPort.getText());
+				login(name, address, port);
+			}
+		});
+		btnLogin.setBounds(91, 311, 117, 29);
+
+		contentPane.add(txtName);
+		contentPane.add(lblName);
+		contentPane.add(txtAddress);
+		contentPane.add(lblIpAddress);
+		contentPane.add(txtPort);
+		contentPane.add(lblPort);
+		contentPane.add(lblAddressDesc);
+		contentPane.add(lblPortDesc);
+		contentPane.add(btnLogin);
+	}
+
+	private void login(String name, String address, int port) {
+		dispose();
+		new Client(name, address, port);
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
